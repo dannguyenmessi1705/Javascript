91e787e2654f08145ffc845d15d587c5b69d6e93
dannguyenmessi1705 <messiprohy@gmail.com>
2023-07-13 02:50:12 +0700

    Javascript

--- Promise/Fetch/Fetch.js
+++ Promise/Fetch/Fetch.js
@@ -1,15 +1,38 @@
 // *** fecth(URL API) -> Dùng để lấy dữ liệu từ API trả về
 // fetch là hàm hoạt động như Promise, nó sẽ trả về 1 object dưới dạng JSON
-// Để trả về kiểu Javascript ta gọi phương thức json() cho nó, nó sẽ tiếp tục trả về 1 promise khác có chứa dữ liệu cần GET
+// Để trả về kiểu Javascript ta gọi phương thức json() (response.json() = JSON.parse(data)), nó sẽ trả về 1 promise và tiếp tục trả về 1 hàm có chứa dữ liệu để ta xử lý dữ liệu đó
 // URL API lấy ví dụ từ trang web jsonplaceholder "https://jsonplaceholder.typicode.com/posts"
-const FETCH = () => {
+
+// *** GET METHOD
+const FetchGet = () => {
   const URL = "https://jsonplaceholder.typicode.com/posts";
   fetch(URL)
     .then((response) => {
+      // Trả về 1 promise
       return response.json();
     })
     .then((json) => {
+      // Hàm trả về kiểu JS
       return console.log(json);
     });
 };
-FETCH();
+FetchGet();
+
+// *** POST METHOD
+const FetchPost = () => {
+  fetch(URL, {
+    method: "POST", // *GET, POST, PUT, DELETE, etc.
+    mode: "cors", // no-cors, *cors, same-origin
+    cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: "same-origin", // include, *same-origin, omit
+    headers: {
+      "Content-Type": "application/json",
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    redirect: "follow", // manual, *follow, error
+    referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    body: JSON.stringify(data), // body data type must match "Content-Type" header
+  })
+    .then((response) => response.json()) // Trả về 1 promise
+    .then((data) => console.log(data)); // Hàm trả về kiểu JS giá trị vừa thêm
+};
--- Promise/Fetch/JSON_WEB_SERVER.js
+++ Promise/Fetch/JSON_WEB_SERVER.js
@@ -5,23 +5,122 @@
  D: Delete - Xoá dữ liệu --- DELETE
  cài json-server trên github
 */
-// Đọc dữ liệu
-const JSON_WEB = () => {
-  // Sau khi chạy local host ta được các API
-  const URLuser = "http://localhost:3000/users";
-  const URLcmt = "http://localhost:3000/comments";
-  // Lấy ra user
-  fetch(URLuser)
-    .then((response) => response.json())
-    .then((data) => console.log("users:", data));
-  // Lấy comments ra
-  fetch(URLcmt)
-    .then((response) => response.json())
-    .then((data) => {
-      console.log("comments:", data);
-    });
-};
-JSON_WEB();
 
-// Để thêm, sửa, xoá dữ liệu ta dùng POSTMAN truyền URL/id
+// Sau khi chạy local host ta được các API
+const URL = "http://localhost:3000/post";
+
+// *** READ POST
+const getPost = (callback) => {
+  fetch(URL)
+    .then((resonse) => resonse.json())
+    .then(callback); // trả về 1 hàm chứa dữ liệu từ server để sau này xử lý
+};
+// *** POST content
+const createPost = (data, callback) => {
+  let option = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data), // Truyền dữ liệu JSON cho web
+  };
+  fetch(URL, option)
+    .then((response) => response.json()) // Trả về 1 promise
+    .then(callback); // Trả về hàm có chữa dữ liệu vừa được thêm để sau này xử lý
+};
+
+// Hàm xử lý bắt sự kiện nút Submit để lấy dữ liệu cả thẻ Input Page và Input Content và thêm 1 dữ liệu mới
+const handleSubmit = () => {
+  const Submit = document.getElementById("submit");
+  Submit.onclick = () => {
+    const page = document.querySelector('input[name="page"]').value; // Lấy dữ liệu từ thẻ input Page
+    const content = document.querySelector('input[name="content"]').value; // Lấy dữ liệu từ thẻ input Content
+    const data = {
+      page: page,
+      content: content,
+    }; // Trả về dạng object có các key như giá trị ban đầu vừa GET
+    createPost(data, Render); // Tạo dữ liệu mới và render ra giá trị vừa nhận
+  };
+};
+
+// *** DELETE Post
+const handleDelete = (id) => {
+  let option = {
+    method: "DELETE",
+  };
+  fetch(URL + "/" + id, option)
+    .then((response) => response.json()) // Trả về 1 promise
+    .then(() => {
+      const PostId = document.querySelector(`#post-id-${id}`);
+      if (PostId) {
+        // Nếu tìm được, thì xoá DOM
+        PostId.remove();
+      }
+    }); // Trả về hàm thực hiện chức năng, ta định nghĩa cho nó thành xoá
+};
+
+// *** UPDATE Post
+const updatePost = (id, data, callback) => {
+  let option = {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data), // Truyền dữ liệu JSON cho web
+  };
+  fetch(URL + "/" + id, option)
+    .then((response) => response.json()) // Trả về 1 promise
+    .then(callback); // Trả về hàm có chữa dữ liệu vừa được thêm để sau này xử lý
+};
+// Hàm xử lý khi ấn nút Edit, các giá trị nó sẽ nhảy vào ô input ở dưới, và chỉ cần Submit là cập nhật thành công
+const handleUpdate = (id) => {
+  // Lấy giá trị của thẻ chứa tên page
+  const copPage = document.getElementById(`page-${id}`).innerText;
+  // Lấy giá trị của thẻ chứa nội dung content
+  const copContent = document.getElementById(`content-${id}`).innerText;
+  // Cho thẻ input Page và Content = với 2 giá trị vừa get
+  document.querySelector('input[name="page"]').value = copPage;
+  document.querySelector('input[name="content"]').value = copContent;
+  // Bắt sự kiện nút Submit để cập nhật
+  const Submit = document.getElementById("submit");
+  Submit.onclick = () => {
+    const page = document.querySelector('input[name="page"]').value; // Lấy dữ liệu từ thẻ input Page
+    const content = document.querySelector('input[name="content"]').value; // Lấy dữ liệu từ thẻ input Content
+    const data = {
+      page: page,
+      content: content,
+    }; // Trả về dạng object có các key như giá trị ban đầu vừa GET
+    updatePost(id, data, Render);
+  };
+};
+
+// Render dữ liệu ra HTML, posts -> Là dữ liệu được lấy từ API
+// posts này lấy từ .then(posts => console.log(posts)) của fetch => Render chính là hàm trả về posts
+const Render = (posts) => {
+  const Element = document.getElementById("post");
+  let html = "";
+  posts.forEach((post) => {
+    html += `
+    <div id="post-id-${post.id}">
+      <h2 id="page-${post.id}">${post.page}</h2>
+      <p id="content-${post.id}">${post.content}</p>
+      <div stlye="display:inline-block">
+        <button id="Delete" onclick="handleDelete(${post.id})">Delete</button>
+        <button id="Edit" onclick="handleUpdate(${post.id})">Edit</button>
+      </div>
+    </div>
+    `;
+  });
+  Element.innerHTML = html;
+};
+const start = () => {
+  getPost(Render);
+  // == getPost((post) => {
+  //      Render(post);
+  //    });
+  handleSubmit();
+};
+start();
+
+// Có thể thêm, sửa, xoá dữ liệu ta dùng POSTMAN truyền URL/id
 // Hoặc ta cài extensions REST Client về rồi tạo file .rest
