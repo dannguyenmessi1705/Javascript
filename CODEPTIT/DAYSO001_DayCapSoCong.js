d37317a1411819ee0a3919b4de4ab4917e814e75
dannguyenmessi1705 <messiprohy@gmail.com>
2023-08-06 19:08:56 +0700

    CODE

--- CODEPTIT/DAYSO001_DayCapSoCong.js
+++ CODEPTIT/DAYSO001_DayCapSoCong.js
@@ -3,42 +3,89 @@ const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
-const check = (arr, csc) => {
-  let newarr = [...arr];
-  while (newarr.length > 1) {
-    let [f, s] = newarr;
-    if (+s - +f != csc) return false;
-    newarr.shift();
+
+const sumArray = (arr) => {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += +arr[i];
   }
-  return true;
+  return sum.toString();
 };
 
 const main = () => {
-  rl.question("", (test) => {
-    test = parseInt(test);
-    let curTest = 1;
-    let result = [];
-    const exec = () => {
-      if (curTest <= test) {
-        rl.question("", (n) => {
-          rl.question("", (arr) => {
-            let Arr = arr.split(" ");
-            Arr.shift();
-            let [f, s] = Arr;
-            let csc = +s - +f;
-            result.push(check(Arr, csc) ? "YES" : "NO");
-          });
-          curTest++;
-          exec();
-        });
+  rl.question("Enter the number of testcases: ", (testcases) => {
+    testcases = parseInt(testcases);
+
+    if (isNaN(testcases) || testcases <= 0) {
+      console.log(
+        "Invalid input. The number of testcases must be a positive integer."
+      );
+      rl.close();
+      return;
+    }
+
+    const exec = (currentTestcase) => {
+      if (currentTestcase <= testcases) {
+        rl.question(
+          "Enter the number of elements (n) for testcase " +
+            currentTestcase +
+            ": ",
+          (n) => {
+            n = parseInt(n);
+
+            if (isNaN(n) || n <= 0) {
+              console.log(
+                "Invalid input. The number of elements (n) must be a positive integer."
+              );
+              exec(currentTestcase);
+              return;
+            }
+
+            let a = [];
+
+            const readElements = (count) => {
+              if (count < n) {
+                rl.question(
+                  `Enter element ${
+                    count + 1
+                  } for testcase ${currentTestcase}: `,
+                  (input) => {
+                    let element = input;
+                    if (isNaN(element)) {
+                      console.log(
+                        "Invalid input. Please enter a valid integer."
+                      );
+                      readElements(count);
+                      return;
+                    }
+
+                    a.push(element);
+                    readElements(count + 1);
+                  }
+                );
+              } else {
+                console.log("Array for testcase " + currentTestcase + ":", a);
+                const totalSum = sumArray(a);
+                console.log(
+                  "Total sum of the elements for testcase " +
+                    currentTestcase +
+                    ":",
+                  totalSum
+                );
+                exec(currentTestcase + 1);
+              }
+            };
+
+            readElements(0);
+          }
+        );
       } else {
-        result.forEach((r) => {
-          console.log(r);
-        });
         rl.close();
       }
     };
-    exec();
+
+    exec(1);
   });
 };
+
 main();
--- Input.js
+++ Input.js
@@ -3,15 +3,24 @@ const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
-rl.question("What is your number testcase ", (answer) => {
-  let n = parseInt(answer); // Chuyen ve kieu du lieu so
-  for (let i = 0; i < n; i++) {
-    rl.on("line", (input) => {
-      const [a, b] = input.split(" ").map(Number);
-      console.log(a + b);
-    });
-  }
-  rl.close();
+rl.question("What is your number testcase ", (test) => {
+  let n = parseInt(test); // Chuyen ve kieu du lieu so
+  let result = [];
+  const exec = (curTest) => {
+    if (curTest <= test) {
+      rl.question("", (input) => {
+        const [a, b] = input.split(" ").map(Number);
+        result.push(a + b);
+        exec(curTest + 1);
+      });
+    } else {
+      result.forEach((r) => {
+        console.log(r);
+      });
+      rl.close();
+    }
+  };
+  exec(1);
 });
 
 // rl.on("line", (input) => {
